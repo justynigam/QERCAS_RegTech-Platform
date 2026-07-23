2684a82d2c1dfa0342da1d6acc26517eba037b3a
justynigam <76672901+justynigam@users.noreply.github.com>
2026-07-24 00:08:28 +0530

    Add STATIC_ROOT, CI/CD workflow, Docker files and settings fixes

--- backend/qercas_project/qercas_project/settings.py
+++ backend/qercas_project/qercas_project/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173",
                           ]
 
